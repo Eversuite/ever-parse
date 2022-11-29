6fb22daa5e003fd876573d3c06075de3b87fe3a5
Dean Lozo <dean.lozo+github@gmail.com>
2022-11-29 21:23:25 +0100

    Write images to folder

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"github.com/gosimple/slug"
 	"github.com/tidwall/gjson"
-	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	FilePathWalkDir(".")
@@ -39,18 +44,17 @@ type AbilityInfo struct {
 
 func FilePathWalkDir(root string) {
 	abilities := make([]AbilityInfo, 0)
-	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if strings.HasPrefix(info.Name(), "BP_UIAbility") {
 			content, err := os.ReadFile(path)
-			if err != nil {
-				log.Fatal("Error when opening file: ", err)
-			}
+			check(err)
 			//Parse the ability mappings
 			abilityRawJson := gjson.Get(string(content), "#(Type%\"BP_UIAbility*\")#|0.Properties").String()
 			var abilityMapping AbilityMapping
-			json.Unmarshal([]byte(abilityRawJson), &abilityMapping)
+			err = json.Unmarshal([]byte(abilityRawJson), &abilityMapping)
+			check(err)
 			//fmt.Println(path)
-			//Check that it uses references for it's data
+			//Check that it uses references for its data
 			if abilityMapping.AbilityName.TableId != "" {
 				name := getAbilityReferenceValue(abilityMapping.AbilityName)
 				id := slug.Make(name)
@@ -59,25 +63,43 @@ func FilePathWalkDir(root string) {
 					name,
 					getAbilityReferenceValue(abilityMapping.AbilityDescription)}
 				abilities = append(abilities, abilityInfo)
+				copyImageFile(abilityMapping.AbilityIcon, id)
 			}
+
 		}
 		return nil
 	})
+	check(err)
+
 	f, _ := os.Create("test.json")
 	enc := json.NewEncoder(f)
 	enc.SetEscapeHTML(false)
 	enc.SetIndent("", " ")
-	enc.Encode(abilities)
-	f.Close()
+	err = enc.Encode(abilities)
+	check(err)
+	err = f.Close()
+	check(err)
+}
+
+func fixRoot(path string) string {
+	return strings.ReplaceAll(path, "/Game/", "Game/")
 }
 
 func getAbilityReferenceValue(abilityReference AbilityReference) string {
-	correctRoot := strings.ReplaceAll(abilityReference.TableId, "/Game", "Game")
+	correctRoot := fixRoot(abilityReference.TableId)
 	regex := regexp.MustCompile("\\..*")
 	cleanedPath := regex.ReplaceAllString(correctRoot, ".json")
 	content, err := os.ReadFile(cleanedPath)
-	if err != nil {
-		log.Fatal("Error when opening file: ", err)
-	}
+	check(err)
 	return gjson.Get(string(content), "#.StringTable.KeysToMetaData."+abilityReference.Key+"|0").String()
+}
+
+func copyImageFile(abilityIcon AbilityIcon, id string) {
+	correctRoot := fixRoot(abilityIcon.ObjectPath)
+	regex := regexp.MustCompile("\\..*")
+	cleanedPath := regex.ReplaceAllString(correctRoot, ".png")
+	content, err := os.ReadFile(cleanedPath)
+	check(err)
+	err = os.WriteFile("./icons/"+id+".png", content, 0644)
+	check(err)
 }
