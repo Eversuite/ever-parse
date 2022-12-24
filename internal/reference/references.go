5ddbfecc12241feb866adbbdb14cfa5c941e8c65
Lukas Spillner <spillnerlu+github@gmail.com>
2022-12-24 17:42:39 +0100

    Implemented image cropping of images with an aspect ratio > 4000.

--- internal/reference/references.go
+++ internal/reference/references.go
@@ -145,7 +145,7 @@ func CopyImageFile(abilityIcon ObjectReference, id string, paths ...string) {
 	dir, err := util.CreateDir(paths...)
 	util.Check(err)
 
-	stuff, didCrop := imgStuff(cleanedPath)
+	stuff, didCrop := cropImage(cleanedPath)
 	croppedFileName := filepath.Join(dir, id+"-cropped.png")
 
 	croppedFile, err := os.OpenFile(croppedFileName, os.O_CREATE|os.O_RDWR, 0644)
@@ -153,6 +153,7 @@ func CopyImageFile(abilityIcon ObjectReference, id string, paths ...string) {
 	if err == nil && didCrop {
 		err = png.Encode(croppedFile, stuff)
 		util.CheckWithoutPanic(err, "Could not safe file")
+		return
 	}
 
 	file := filepath.Join(dir, id+".png")
